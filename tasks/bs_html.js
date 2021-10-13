07a756abffd3997ac3a60035adc1d4e8bfbbea06
TiroZit <nikus.master@gmail.com>
2021-10-13 15:54:14 +0500

    init

---
+++ tasks/bs_html.js
@@ -0,0 +1,25 @@
+const bs = require('browser-sync');
+
+module.exports = function bs_html() {
+	bs.init({
+		server: {
+			baseDir: 'build/',
+			host: '192.168.100.3',
+		},
+		callbacks: {
+			ready: function (err, bs) {
+				bs.addMiddleware("*", function (req, res) {
+					res.writeHead(302, {
+						location: "404.html"
+					});
+					res.end("Redirecting!");
+				});
+			}
+		},
+		logPrefix: 'BS-HTML:',
+		logLevel: 'info',
+		logConnections: true,
+		logFileChanges: true,
+		open: true
+	})
+}
---
+++ tasks/bs_php.js
@@ -0,0 +1,12 @@
+const bs = require('browser-sync');
+
+module.exports = function bs_php() {
+	bs.init({
+		watch: true,
+		proxy: '',
+		logLevel: 'info',
+		logPrefix: 'BS-PHP:',
+		logConnections: true,
+		logFileChanges: true,
+	})
+}
