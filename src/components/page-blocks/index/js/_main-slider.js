fac948a40c1c40e8bd14b67f608fa9e9c4a9b1b4
TiroZit <nikus.master@gmail.com>
2021-10-21 19:08:05 +0500

    main-slider

---
+++ src/components/page-blocks/index/js/_main-slider.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.main-slider__container', {
+   grabCursor: true,
+   loop: true,
+   speed: 500,
+   pagination: {
+     el: '.swiper-pagination',
+     clickable: true,
+     renderBullet: function (index, className) {
+      return `<div class="swiper-pagination-bullet"><div></div></div>`;
+    },
+
+   },
+   navigation: {
+     nextEl: '.swiper-button-next',
+     prevEl: '.swiper-button-prev',
+   },
+ });
