eccc93b1c83e0d4dcc59d4d1fef501e2c13cad7a
TiroZit <nikus.master@gmail.com>
2021-10-24 20:55:45 +0500

    leisure

---
+++ src/components/page-blocks/index/js/_tabs.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('.tabs__link').forEach((item) =>
+   item.addEventListener('click', function (e) {
+      e.preventDefault();
+      const id = e.target.getAttribute('href').replace('#', '');
+
+      document.querySelectorAll('.tabs__link').forEach((child) => child.classList.remove('active')
+      );
+      document.querySelectorAll('.leisure__list').forEach((child) => child.classList.remove('active')
+      );
+      item.classList.add('active');
+      document.getElementById(id).classList.add('active');
+   })
+);
+document.querySelector('.tabs__link').click();
