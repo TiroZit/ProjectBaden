ca207cd7af6a6f980e174937b9484134f8d15cc1
TiroZit <nikus.master@gmail.com>
2021-11-01 18:03:18 +0500

    booking

--- src/components/page-blocks/index/js/_spoiler.js
+++ src/components/page-blocks/index/js/_spoiler.js
@@ -3,8 +3,16 @@ document.querySelectorAll('.faq__question').forEach((item) =>
       item.classList.toggle('open');
    })
 )
-document.querySelectorAll('.booking__item').forEach((item) => 
-   item.addEventListener('click', () => {
-      item.classList.toggle('open');
+const spoilers = document.querySelectorAll('.booking__item');
+for(item of spoilers){
+   item.addEventListener('click', function () {
+      if(this.classList.contains('open')){
+         this.classList.remove('open');
+      } else{
+         for (el of spoilers){
+            el.classList.remove('open');
+         }
+         this.classList.add('open');
+      }
    })
-)
+}
