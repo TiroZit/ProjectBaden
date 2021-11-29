e26781f97c175cce53a76202fa21560f3a9ffb0f
TiroZit <nikus.master@gmail.com>
2021-11-29 20:09:58 +0500

    room-slider

---
+++ src/js/components/room-slider.js
@@ -0,0 +1,14 @@
+const mainSlider = new Swiper('.room-slider', {
+  grabCursor: true,
+  loop: true,
+  speed: 500,
+  preloadImages: false,
+  lazy: {
+    loadPrevNext: true,
+  },
+  slidesPerView: 4,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
