52d27b02852534d8afc295e6616fcfa240a8925a
TiroZit <nikus.master@gmail.com>
2021-12-02 14:19:51 +0500

    room-slider responsive

--- src/js/components/room-slider.js
+++ src/js/components/room-slider.js
@@ -1,14 +1,46 @@
 const roomSlider = new Swiper('.room-slider', {
-  grabCursor: true,
-  loop: true,
+  loop: false,
   speed: 500,
+  spaceBetween: 30,
   preloadImages: false,
   lazy: {
     loadPrevNext: true,
   },
-  slidesPerView: 4,
+  loadPrevNextAmount: 1,
+  grid: {
+    rows: 1,
+    columns: 1,
+  },
+  slidesPerView: 1,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+  breakpoints: {
+    // when window width is >= 640px
+    1750: {
+      loadPrevNextAmount: 4,
+      slidesPerView: 4,
+      grid: {
+        rows: 2,
+        columns: 4,
+      },
+    },
+    1040: {
+      loadPrevNextAmount: 3,
+      slidesPerView: 3,
+      grid: {
+        rows: 2,
+        columns: 3,
+      },
+    },
+    610: {
+      loadPrevNextAmount: 2,
+      slidesPerView: 2,
+      grid: {
+        rows: 1,
+        columns: 2,
+      },
+    }
+  }
 });
